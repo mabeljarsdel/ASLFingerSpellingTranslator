9f93e009b6f6347b41c3fffc819c771483cca265
Matt Luke <59681386+MlukeDev@users.noreply.github.com>
2020-09-06 18:17:23 -0400

    Initial Commit

---
+++ Sign Translator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sign Translator
+//
+//  Created by Matt Luke on 9/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
