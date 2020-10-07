16ef13fc4353a18f9d3f8e725df769e178f46509
Matt Luke <59681386+MlukeDev@users.noreply.github.com>
2020-10-06 21:46:46 -0400

    Initial Commit (currently broken)

--- Sign Translator/ViewController.swift
+++ Sign Translator/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    @IBOutlet weak var previewView: UIView!
+    
+    
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let modelURL = modelURL = Bundle.main.url(forResource: "ObjectDetector", withExtension: "mlmodelc")
+        let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL))
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewView.layer.addSublayer(previewLayer)
+        previewLayer.frame = previewView.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
     }
-
-
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        
+        
+    }
+    
 }
+
 
